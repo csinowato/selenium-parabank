1aa3e462f0a62a9b7286438527993488289a487e
Claudia Sinowato <claudia.sinowato@gmail.com>
2025-09-29 00:22:33 -0700

    fix: configure Chrome driver for GitHub Actions and run in headless mode

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,23 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver():
+    options = Options()
+
+    # use headless mode if running in GitHub Actions
+    if os.getenv("GITHUB_ACTIONS") == "true":
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
     service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    driver.maximize_window()
+    driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
 
